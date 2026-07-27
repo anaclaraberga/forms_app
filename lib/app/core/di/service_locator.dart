5acdd8c1f0b9a9f26139104f5f35dde239290f66
anaclaraberga <ana4ew.br@gmail.com>
2026-07-26 22:11:04 -0300

    Adicionado: Uses Cases no Get It

--- lib/app/core/di/service_locator.dart
+++ lib/app/core/di/service_locator.dart
@@ -1,8 +1,14 @@
 import 'package:get_it/get_it.dart';
 import '../network/http_client.dart';
 
+import 'package:forms_app/app/features/posts/domain/use_cases/create_post_usecase.dart';
+import 'package:forms_app/app/features/posts/domain/use_cases/get_posts_usecase.dart';
+import 'package:forms_app/app/features/posts/domain/repositories/post_repository.dart';
+
 final sl = GetIt.instance;
 
 void setupServiceLocator() {
   sl.registerLazySingleton<HttpClient>(() => HttpClient(null));
+  sl.registerFactory(() => GetPostsUseCase(sl<PostRepository>()));
+  sl.registerFactory(() => CreatePostUseCase(sl<PostRepository>()));
 }
